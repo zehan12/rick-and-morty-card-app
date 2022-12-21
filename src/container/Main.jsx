5eedbea79a347032d62c46e5aca1155cc42872c9
Zehan Khan <zehan9211@gmail.com>
2022-12-22 00:46:55 +0530

    add main

---
+++ src/container/Main.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react"
+
+const Main = () => {
+    return (
+        <Fragment>
+            <div className="container">
+                <h1>Main app</h1>
+                
+            </div>
+        </Fragment>
+    )
+}
+
+export default Main;
